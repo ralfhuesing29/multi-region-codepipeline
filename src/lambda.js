647f4174f7ac48733a6a40825d81f933f0d2a0fc
ralfhuesing29 <ralf.huesing@icloud.com>
2019-06-27 20:27:22 +0200

    Update

--- src/lambda.js
+++ src/lambda.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
     const message = `Lambda with ${context.memoryLimitInMB}MB memory invoked at ${new Date()}`;
-    console.log("message");
+    console.log("My message");
     console.log("Event was: ");
     console.log(JSON.stringify(event))
     return message;
